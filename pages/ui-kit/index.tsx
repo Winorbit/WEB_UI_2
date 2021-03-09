b9ed93c59f7cf787544adcebfe936bee6825097e
neko <ai73a@yandex.ru>
2021-03-09 17:35:22 +0300

    add ui-kit inputs

--- pages/ui-kit/index.tsx
+++ pages/ui-kit/index.tsx
@@ -1,11 +1,14 @@
 import {Page} from '../../ui-kit/page/Page'
 import s from './ui-kit.module.css'
 import Button from './button/Button'
+import Input from './input/Input'
 
 const UIKit = () => {
     return (
         <Page>
             <div className={s.test}>ui kit</div>
+
+            buttons ----------------------------------
             <Button type={'blue'}>enter</Button>
             <Button type={'whiteBlue'}>else</Button>
             <div style={{padding: 10}}>
@@ -16,6 +19,12 @@ const UIKit = () => {
                 <Button type={'white'}>send</Button>
             </div>
             <Button type={'white'} disabled>disabled</Button>
+
+            inputs -----------------------------------
+            <Input headText={'def'} bottomText={'please, input...'}/>
+            <Input headText={'ok'} status={'ok'}/>
+            <Input headText={'error'} bottomText={'some error'} status={'error'}/>
+            <Input headText={'password'} bottomText={'input pass...'} type={'password'}/>
         </Page>
     )
 }
---
+++ pages/ui-kit/input/Input.tsx
@@ -0,0 +1,52 @@
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import s from './Input.module.css'
+
+type DefInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type InputPropsType = Omit<DefInputPropsType, 'type'> & {
+    type?: 'password',
+    status?: 'ok' | 'error',
+    headText?: string,
+    bottomText?: string,
+    onChangeText?: (text: string) => void,
+}
+
+const Input: React.FC<InputPropsType> = (
+    {
+        className, children,
+        status, headText, bottomText, onChange,
+        onChangeText,
+        ...restProps
+    }
+) => {
+    const finalClassName = (
+        s[status || 'def']
+    ) + (
+        className
+            ? ' ' + className
+            : ''
+    )
+
+    const finalBottomTextClassName = status === 'error'
+        ? s.errorBottomText
+        : s.bottomText
+
+    const onChangeCallBack = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            <div className={s.headText}>{headText || <br/>}</div>
+            <input
+                className={finalClassName}
+                onChange={onChangeCallBack}
+                {...restProps}
+            />
+            <div className={finalBottomTextClassName}>{bottomText || <br/>}</div>
+        </div>
+    )
+}
+
+export default Input
