02b85e5318143a7f66b3a25c072b8ba8eb002d27
neko <ai73a@yandex.ru>
2021-03-15 15:49:12 +0300

    header update

---
+++ assets/men-svg.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Men = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.5" y="0.5" width="23" height="23" rx="1.5" stroke="#F9F9F9"/>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M11.6997 21.2728L11.6167 9.5213L5.45459 7.16951L11.6997 21.2728ZM12.1479 9.51731L12.3117 21.1092L18.5455 6.99561L12.1479 9.51731Z"
+                  fill="#F9F9F9"/>
+            <ellipse cx="11.8105" cy="5.57283" rx="2.93359" ry="2.8456" fill="#F9F9F9"/>
+        </svg>
+    )
+}
+
+export default Men
--- ui-kit/header/Header.tsx
+++ ui-kit/header/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import s from './Header.module.css'
+import Logo from './../../assets/logo-svg'
+import Men from './../../assets/men-svg'
 
 export const Header: React.FC = () => {
     return (
@@ -9,13 +11,25 @@ export const Header: React.FC = () => {
                 <div className={s.menuItem}>обучение</div>
                 <div className={s.menuItem}>цены</div>
                 <div className={s.menuItem}>о нас</div>
-                <Link href={'/ui-kit'}>ui-kit</Link>
+
+                {/*for test*/}
+                <div className={s.menuItem}><Link href={'/ui-kit'}>ui-kit</Link></div>
+                <div className={s.menuItem}><Link href={'/'}>home</Link></div>
+                <div className={s.menuItem}><Link href={'/courses'}>courses</Link></div>
+                <div className={s.menuItem}><Link href={'/lesson'}>lesson</Link></div>
             </div>
 
-            <Link href={'/'}>home</Link>
-            <Link href={'/courses'}>courses</Link>
-            <Link href={'/lesson'}>lesson</Link>
+            <div className={s.center}>
+                <div className={s.text}>зимняя</div>
+                <Logo/>
+                <div className={s.text}>орбита</div>
+            </div>
 
+            <div className={s.menu}>
+                <div className={s.menuItem}>регистрация</div>
+                <div className={s.menuItemEnd}>войти</div>
+                <div className={s.men}><Men/></div>
+            </div>
         </header>
     )
 }
