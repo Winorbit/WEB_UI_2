c57be14ae536ba5de39bc005e7bf06c524d6d265
neko <ai73a@yandex.ru>
2021-03-15 16:36:13 +0300

    add spaces to header

--- ui-kit/header/Header.tsx
+++ ui-kit/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
+// import Link from 'next/link'
 import s from './Header.module.css'
 import Men from './../../assets/men-svg'
 import {WinOrbit} from '../win-orbit/WinOrbit'
@@ -13,13 +13,17 @@ export const Header: React.FC = () => {
                 <div className={s.menuItem}>о нас</div>
 
                 {/*for test*/}
-                <div className={s.menuItem}><Link href={'/ui-kit'}>ui-kit</Link></div>
-                <div className={s.menuItem}><Link href={'/'}>home</Link></div>
-                <div className={s.menuItem}><Link href={'/courses'}>courses</Link></div>
-                <div className={s.menuItem}><Link href={'/lesson'}>lesson</Link></div>
+                {/*<div className={s.menuItem}><Link href={'/ui-kit'}>ui-kit</Link></div>*/}
+                {/*<div className={s.menuItem}><Link href={'/'}>home</Link></div>*/}
+                {/*<div className={s.menuItem}><Link href={'/courses'}>courses</Link></div>*/}
+                {/*<div className={s.menuItem}><Link href={'/lesson'}>lesson</Link></div>*/}
             </div>
 
+            <div className={s.space}/>
+
             <WinOrbit/>
+
+            <div className={s.space}/>
 
             <div className={s.menu}>
                 <div className={s.menuItem}>регистрация</div>
