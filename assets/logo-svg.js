15784e3c75767913a76a880e32435d3d48d52b9f
neko <ai73a@yandex.ru>
2021-03-22 13:38:56 +0300

    fix svg

--- assets/logo-svg.js
+++ assets/logo-svg.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Logo = () => {
     return (
         <svg width="56" height="59" viewBox="0 0 56 59" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="28" cy="31" r="27" stroke="#F9F9F9" stroke-width="2"/>
+            <circle cx="28" cy="31" r="27" stroke="#F9F9F9" strokeWidth="2"/>
             <circle cx="28" cy="4" r="4" fill="#F9F9F9"/>
             <path d="M27 55V29L12 23L27 55Z" fill="#F9F9F9"/>
             <path d="M29 55V29L44 23L29 55Z" fill="#F9F9F9"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M44 21L28 9L12 21H44ZM12 21L28 27L44 21H12Z"
+            <path fillRule="evenodd" clipRule="evenodd" d="M44 21L28 9L12 21H44ZM12 21L28 27L44 21H12Z"
                   fill="#F9F9F9"/>
         </svg>
     )
